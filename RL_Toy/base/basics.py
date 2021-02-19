d78bfdeac03366e7b940905a9802d65bf757cc5b
ROB <robertolopez94@outlook.com>
2021-02-19 15:53:05 -0700

    missed definition

--- RL_Toy/base/basics.py
+++ RL_Toy/base/basics.py
@@ -45,99 +45,6 @@ class ActionSpace(ABC):
         """
         return np.random.randint(self.mV, self.mV + self.n)
 
-class ObservationSpace(ABC):
-    """
-    Discrete observation space class
-
-    Stores all the posible observations for an environment and can generate
-    samples with the method sample().
-
-    Parameters
-    ----------
-    env: Environment
-        All the object
-    minValue: int
-        Default 0. The minimum value that a cell's state can take. It's
-        the lower inclusive of the action space intervals 
-        [minValue, minValue + n)
-    """
-    def __init__(self, env:Environment, minValue:int = 0):
-        if isinstance(n, int):
-            n = (n,)
-        assert len(n) > 0, "Tuple must contain at least 1 item"
-        self.env = env
-        self._n_ = env.shape
-        self.mV = minValue
-
-    def __iter__(self):
-        self._i_ = []
-        self._tl_ = []
-        for i in self._n_:
-            ax = abs(i - self.mV)
-            self._tl_+= [ax - 1] # Max items on this position
-            self._i_ += [0] # For actual counter
-        self._ii_, self._is_ = 1, 1 # Index position inferior, superior
-        return self
-
-    def __next__(self):
-        
-        def incSupr():
-            nonlocal self
-            if self._i_[self._is_] == (self._tl_[self._is_]):
-                self._i_[self._is_] = 0 # reset superior index
-                self._is_ += 1 # increment one
-                if self._is_ == len(self._n_):
-                    raise StopIteration
-                incSupr()
-            else:
-                self._i_[self._is_] += 1
-                self._is_ = 1
-
-        def zeroCnt():
-            nonlocal self
-            if self._i_[0] == (self._tl_[0]):
-                self._i_[0] = 0 # Reset the low index
-                incSupr()
-            else:
-                self._i_[0] += 1
-
-        def doTpl():
-            nonlocal self
-            tupl = []
-            for i in self._i_:
-                tupl += [self.mV + i]
-            return tuple(tupl)
-
-        if len(self._n_) < 2:
-                if self._i_[0] <= (self._tl_[0]):
-                    t = doTpl()
-                    self._i_[0] += 1
-                    return t
-                else:
-                    raise StopIteration
-        else:
-            zeroCnt()
-        tpl = doTpl()
-        if not self.env.isValid(tpl):
-            return __next__()
-            
-        return tpl
-
-
-    @property
-    def shape(self):
-        return self._n_
-
-    def sample(self):
-        """
-        Returns a random sample with an uniform distribution of the
-        cell's states.
-        """
-        newState = []
-        for i in self._n_:
-            newState += [np.random.randint(self.mV, self.mV + i)]
-        return newState
-
 
 class Environment(ABC):
     """
@@ -218,3 +125,95 @@ class Policy(ABC):
         Update the action per state manner of the policy
         """
         raise NotImplementedError
+
+class ObservationSpace(ABC):
+    """
+    Discrete observation space class
+
+    Stores all the posible observations for an environment and can generate
+    samples with the method sample().
+
+    Parameters
+    ----------
+    env: Environment
+        All the object
+    minValue: int
+        Default 0. The minimum value that a cell's state can take. It's
+        the lower inclusive of the action space intervals 
+        [minValue, minValue + n)
+    """
+    def __init__(self, env:Environment, minValue:int = 0):
+        if isinstance(n, int):
+            n = (n,)
+        assert len(n) > 0, "Tuple must contain at least 1 item"
+        self.env = env
+        self._n_ = env.shape
+        self.mV = minValue
+
+    def __iter__(self):
+        self._i_ = []
+        self._tl_ = []
+        for i in self._n_:
+            ax = abs(i - self.mV)
+            self._tl_+= [ax - 1] # Max items on this position
+            self._i_ += [0] # For actual counter
+        self._ii_, self._is_ = 1, 1 # Index position inferior, superior
+        return self
+
+    def __next__(self):
+        
+        def incSupr():
+            nonlocal self
+            if self._i_[self._is_] == (self._tl_[self._is_]):
+                self._i_[self._is_] = 0 # reset superior index
+                self._is_ += 1 # increment one
+                if self._is_ == len(self._n_):
+                    raise StopIteration
+                incSupr()
+            else:
+                self._i_[self._is_] += 1
+                self._is_ = 1
+
+        def zeroCnt():
+            nonlocal self
+            if self._i_[0] == (self._tl_[0]):
+                self._i_[0] = 0 # Reset the low index
+                incSupr()
+            else:
+                self._i_[0] += 1
+
+        def doTpl():
+            nonlocal self
+            tupl = []
+            for i in self._i_:
+                tupl += [self.mV + i]
+            return tuple(tupl)
+
+        if len(self._n_) < 2:
+                if self._i_[0] <= (self._tl_[0]):
+                    t = doTpl()
+                    self._i_[0] += 1
+                    return t
+                else:
+                    raise StopIteration
+        else:
+            zeroCnt()
+        tpl = doTpl()
+        if not self.env.isValid(tpl):
+            return __next__()
+            
+        return tpl
+
+    @property
+    def shape(self):
+        return self._n_
+
+    def sample(self):
+        """
+        Returns a random sample with an uniform distribution of the
+        cell's states.
+        """
+        newState = []
+        for i in self._n_:
+            newState += [np.random.randint(self.mV, self.mV + i)]
+        return newState
