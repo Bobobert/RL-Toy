1659241da24ee587efd09fca3daa1f349552f219
Bobobert <50603306+Bobobert@users.noreply.github.com>
2021-03-08 21:09:10 -0600

    Update __init__.py

--- RL_Toy/policies/__init__.py
+++ RL_Toy/policies/__init__.py
@@ -1,2 +1,2 @@
 from RL_Toy.policies.simples import uniformRandomPolicy, gridPolicy, gridPolicyEpsilon
-from RL_Toy.policies.gym import gymPolicy
+from RL_Toy.policies.gym import gymPolicy, gymPolicyDiscreteFromCon
