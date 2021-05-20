34d5955525aa761b3180181168b8a9b83061623f
Bobobert <50603306+Bobobert@users.noreply.github.com>
2021-05-20 03:14:02 -0500

    Update __init__.py

--- RL_Toy/utils/__init__.py
+++ RL_Toy/utils/__init__.py
@@ -1,3 +1,3 @@
-from RL_Toy.utils.utils import runEnv, render, runPolicy
+from RL_Toy.utils.utils import runEnv, render, runPolicy, runAgent
 from RL_Toy.utils.functions import Q_function, checkForTuple
 from RL_Toy.utils.vars import Variable, linearSchedule
